792c6fe57af683765326f5fa924e61ffaa3400f4
FingolfinTEK <fingolfintek@gmail.com>
2015-02-15 20:30:51 +0100

    Using bower for JS dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,40 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         bower_concat: {
             all: {
-                dest: 'build/_bower.js',
-                cssDest: 'build/_bower.css'//,
-                //bowerOptions: {
-                //    relative: false
-                //}
+                dest: 'build/bower.js',
+                cssDest: 'build/bower.css'
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'build/bower.min.js': ['build/bower.js']
+                }
+            }
+        },
+        cssmin: {
+            //options: {
+            //    shorthandCompacting: false,
+            //    roundingPrecision: -1
+            //},
+            target: {
+                files: {
+                    'build/bower.min.css': ['build/bower.css']
+                }
             }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['bower_concat', 'uglify', 'cssmin']);
 
 };
